98a2bdf79b7b7dcabf5c14ae882bdd87df2915f3
Seviche Cen <sevicheee@outlook.com>
2022-03-03 17:57:52 +0800

    JS没完成

---
+++ Day 03-04/JavaScript/script.js
@@ -0,0 +1,49 @@
+'use strict';
+//////////////////////////////函数基础
+//////TASK 1
+
+function showMessage1() {
+  return 'hello,stranger!';
+}
+console.log(showMessage1());
+
+///////TASK 2
+
+function showMessage2(name) {
+  return `hello,${name}!`;
+}
+document.write(showMessage2('Jack'));
+
+//////////////////////////////函数表达式 & 回调函数
+
+//////Task 1
+const showMessage = function (name) {
+  return `hello,${name}3`;
+};
+
+//1
+// setTimeout(
+//     (function(name) {
+//         document.write(showMessage3(name));;
+//     }), 1000, 'Jack')
+
+//2
+setTimeout(() => {
+  document.write(showMessage('Jack'));
+}, 1000);
+
+/////Task2
+
+const success = function () {
+  document.write(`体温正常，打卡成功！`);
+};
+const fail = function () {
+  document.write(`体温异常，打卡失败！`);
+};
+
+function temperatureBodyDetection(success, fail) {
+  const temper = Number(prompt('请输入您的体温'));
+  if (temper <= 38 && temper >= 36) success();
+  /*执行成功函数*/ else fail(); /*执行失败函数*/
+}
+temperatureBodyDetection(success, fail); /**调用打卡函数**/
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # IFE-Practice
 百度前端学院练习
 http://ife.baidu.com/
+
+开始日期：2022/2/25 
+
 ## Day 01-02
 - HTML:[文字高级格式化——博文](http://ife.baidu.com/htmlpart/text.html)
 
@@ -8,10 +11,18 @@ http://ife.baidu.com/
 
 - JavaScript:[Javascript 基础知识（一）](http://ife.baidu.com/javascript/datatype&variable.html#null-%E5%92%8C-undefined)
 
-## Day 03
+## Day 03-04
 
 - HTML:[制作一个奶茶点餐页面](http://ife.baidu.com/htmlpart/form.html)
 
 - CSS: [精准定位你想美化的元素 ](http://ife.baidu.com/csspart/selector.html)
 
 - JavaScript:[使用函数创建模块化代码](http://ife.baidu.com/javascript/function.html#%E5%87%BD%E6%95%B0%E8%A1%A8%E8%BE%BE%E5%BC%8F-%E5%9B%9E%E8%B0%83%E5%87%BD%E6%95%B0)
+
+## Day 05-06
+
+- HTML:[响应式图像](http://ife.baidu.com/htmlpart/responsiveImage.html)
+
+- CSS:[制作一些有趣的效果](http://ife.baidu.com/csspart/playWithCss.html)
+
+- JavaScript:[“如果” 可以 “重来”](http://ife.baidu.com/javascript/if&while.html)
